fd88b69573df9524a3905d85c4a5b28bc8e9a9e8
Sasha Petra <sasha.petra96@gmail.com>
2023-06-27 11:11:11 +0200

    translator provider

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,18 +1,29 @@
 import {h, ComponentProps, ComponentChildren, createContext} from 'preact'
+import TranslationController from './controllers'
+import type {TranslationType} from './controllers'
 
 type TranslationProviderProps = {
-	children: ComponentChildren
+	children: ComponentChildren,
+	translations: TranslationType[],
+	languages: string[],
+	selectedLanguage: string
 }
 
-export const TranslationContext =  createContext(new Map())
+export const TranslationContext =  createContext<TranslationController>( new TranslationController() as any as TranslationContextType)
 
 export default function TranslationProvider(props:TranslationProviderProps) {
 
-	const translations = new Map()
-	translations.set("ciao", "working")
+	console.log(TranslationContext)
+
+	const context = new TranslationController()
+	
+	context.setLanguages(props.languages)
+	context.setTranslations(props.translations)
+	
+	context.setSelectedLanguage(props.selectedLanguage ?? props.languages[0])
 
 	
-	return <TranslationContext.Provider value={translations}>
+	return <TranslationContext.Provider value={context}>
 		{props.children}
 	</TranslationContext.Provider>
 }
